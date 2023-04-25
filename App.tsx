48bda87145b5299a707503b0496853ace2283a43
shivaminfowind <shivam.infowind@gmail.com>
2023-04-25 14:21:13 +0530

    commonBottomModal designing completed.

--- App.tsx
+++ App.tsx
@@ -4,13 +4,16 @@ import Rootnavigation from './src/navigations/Rootnavigation';
 import {NavigationContainer} from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
 
 function App(): JSX.Element {
   return (
     <>
     <GestureHandlerRootView style={{flex:1}}>
     <NavigationContainer>
+      <BottomSheetModalProvider>
         <Rootnavigation />
+        </BottomSheetModalProvider>
       </NavigationContainer>
     </GestureHandlerRootView>
     
